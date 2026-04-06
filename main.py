e5ef0b8a4f2ee637cc77a61521a7e0285a1c0fe9
Vera Gonzalez <me@vera.lgbt>
2026-04-06 15:05:01 -0400

    dedupe

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import nirukta.patches  # pyright: ignore[reportUnusedImport]
 
+from janim.imports import RED, Timeline
+from nirukta.util import is_nirukta_file, file_to_timeline
+
 import importlib
 
-from janim.imports import Config, Timeline
-
-from nirukta.nirukta import Nirukta
+from janim.imports import Config
 from nirukta.util import choose_nirukta_file
 
 importlib.reload(nirukta)
@@ -12,9 +13,16 @@ importlib.reload(nirukta)
 chosen = choose_nirukta_file()
 
 
-class EntryPoint(Timeline):
-    CONFIG = Config(fps=24, preview_fps=24)
+class Nirukta(Timeline):
+    CONFIG = Config(fps=60, preview_fps=60)
+
+    nirukta: Timeline
+
+    @property
+    def gui_color(self) -> str:
+        return RED
 
     def construct(self):
-        timeline = Nirukta(chosen).build().to_item().show()
+        assert is_nirukta_file(chosen), "Invalid file"
+        timeline = file_to_timeline(chosen).build().to_item().show()
         self.forward_to(timeline.end)
